59a2de87eedbdc9fae554205ea2fa58b97472c4c
Dell Du <18588220928@163.com>
2019-12-24 15:35:17 +0800

    Update.

--- bagnet_arch.py
+++ bagnet_arch.py
@@ -2,24 +2,36 @@ import math
 import torch.nn as nn
 from torch.utils import model_zoo
 
-
 model_urls = {
-    'bagnet9': 'https://bitbucket.org/wielandbrendel/bag-of-feature-pretrained-models/raw/249e8fa82c0913623a807d9d35eeab9da7dcc2a8/bagnet8-34f4ccd2.pth.tar',
-    'bagnet17': 'https://bitbucket.org/wielandbrendel/bag-of-feature-pretrained-models/raw/249e8fa82c0913623a807d9d35eeab9da7dcc2a8/bagnet16-105524de.pth.tar',
-    'bagnet33': 'https://bitbucket.org/wielandbrendel/bag-of-feature-pretrained-models/raw/249e8fa82c0913623a807d9d35eeab9da7dcc2a8/bagnet32-2ddd53ed.pth.tar',
+    'bagnet9':
+    'https://bitbucket.org/wielandbrendel/bag-of-feature-pretrained-models/raw/249e8fa82c0913623a807d9d35eeab9da7dcc2a8/bagnet8-34f4ccd2.pth.tar',
+    'bagnet17':
+    'https://bitbucket.org/wielandbrendel/bag-of-feature-pretrained-models/raw/249e8fa82c0913623a807d9d35eeab9da7dcc2a8/bagnet16-105524de.pth.tar',
+    'bagnet33':
+    'https://bitbucket.org/wielandbrendel/bag-of-feature-pretrained-models/raw/249e8fa82c0913623a807d9d35eeab9da7dcc2a8/bagnet32-2ddd53ed.pth.tar',
 }
 
 
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None, kernel_size=1):
+    def __init__(self,
+                 inplanes,
+                 planes,
+                 stride=1,
+                 downsample=None,
+                 kernel_size=1):
         super(Bottleneck, self).__init__()
         # print('Creating bottleneck with kernel size {} and stride {} with padding {}'.format(kernel_size, stride, (kernel_size - 1) // 2))
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=kernel_size, stride=stride,
-                               padding=0, bias=False) # changed padding from (kernel_size - 1) // 2
+        self.conv2 = nn.Conv2d(
+            planes,
+            planes,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=0,
+            bias=False)  # changed padding from (kernel_size - 1) // 2
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(planes * 4)
@@ -43,11 +55,11 @@ class Bottleneck(nn.Module):
 
         if self.downsample is not None:
             residual = self.downsample(x)
-        
+
         if residual.size(-1) != out.size(-1):
             diff = residual.size(-1) - out.size(-1)
-            residual = residual[:,:,:-diff,:-diff]
-        
+            residual = residual[:, :, :-diff, :-diff]
+
         out += residual
         out = self.relu(out)
 
@@ -55,20 +67,53 @@ class Bottleneck(nn.Module):
 
 
 class BagNet(nn.Module):
-
-    def __init__(self, block, layers, strides=[1, 2, 2, 2], kernel3=[0, 0, 0, 0], num_classes=1000, avg_pool=True):
+    def __init__(self,
+                 block,
+                 layers,
+                 strides=[1, 2, 2, 2],
+                 kernel3=[0, 0, 0, 0],
+                 num_classes=1000,
+                 avg_pool=True):
         self.inplanes = 64
         super(BagNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=1, stride=1, padding=0,
+        self.conv1 = nn.Conv2d(3,
+                               64,
+                               kernel_size=1,
+                               stride=1,
+                               padding=0,
                                bias=False)
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0,
+        self.conv2 = nn.Conv2d(64,
+                               64,
+                               kernel_size=3,
+                               stride=1,
+                               padding=0,
                                bias=False)
         self.bn1 = nn.BatchNorm2d(64, momentum=0.001)
         self.relu = nn.ReLU(inplace=True)
-        self.layer1 = self._make_layer(block, 64, layers[0], stride=strides[0], kernel3=kernel3[0], prefix='layer1')
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=strides[1], kernel3=kernel3[1], prefix='layer2')
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=strides[2], kernel3=kernel3[2], prefix='layer3')
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=strides[3], kernel3=kernel3[3], prefix='layer4')
+        self.layer1 = self._make_layer(block,
+                                       64,
+                                       layers[0],
+                                       stride=strides[0],
+                                       kernel3=kernel3[0],
+                                       prefix='layer1')
+        self.layer2 = self._make_layer(block,
+                                       128,
+                                       layers[1],
+                                       stride=strides[1],
+                                       kernel3=kernel3[1],
+                                       prefix='layer2')
+        self.layer3 = self._make_layer(block,
+                                       256,
+                                       layers[2],
+                                       stride=strides[2],
+                                       kernel3=kernel3[2],
+                                       prefix='layer3')
+        self.layer4 = self._make_layer(block,
+                                       512,
+                                       layers[3],
+                                       stride=strides[3],
+                                       kernel3=kernel3[3],
+                                       prefix='layer4')
         self.avgpool = nn.AvgPool2d(1, stride=1)
         self.fc = nn.Linear(512 * block.expansion, num_classes)
         self.avg_pool = avg_pool
@@ -82,18 +127,32 @@ class BagNet(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
-    def _make_layer(self, block, planes, blocks, stride=1, kernel3=0, prefix=''):
+    def _make_layer(self,
+                    block,
+                    planes,
+                    blocks,
+                    stride=1,
+                    kernel3=0,
+                    prefix=''):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
+                nn.Conv2d(self.inplanes,
+                          planes * block.expansion,
+                          kernel_size=1,
+                          stride=stride,
+                          bias=False),
                 nn.BatchNorm2d(planes * block.expansion),
             )
 
         layers = []
         kernel = 1 if kernel3 == 0 else 3
-        layers.append(block(self.inplanes, planes, stride, downsample, kernel_size=kernel))
+        layers.append(
+            block(self.inplanes,
+                  planes,
+                  stride,
+                  downsample,
+                  kernel_size=kernel))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
             kernel = 1 if kernel3 <= i else 3
@@ -120,10 +179,11 @@ class BagNet(nn.Module):
             x = x.view(x.size(0), -1)
             x = self.fc(x)
         else:
-            x = x.permute(0,2,3,1)
+            x = x.permute(0, 2, 3, 1)
             x = self.fc(x)
 
         return x
+
 
 def create_bagnet33(pretrained=False, strides=[2, 2, 2, 1], **kwargs):
     """Constructs a Bagnet-33 model.
@@ -131,10 +191,14 @@ def create_bagnet33(pretrained=False, strides=[2, 2, 2, 1], **kwargs):
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
-    model = BagNet(Bottleneck, [3, 4, 6, 3], strides=strides, kernel3=[1,1,1,1], **kwargs)
+    model = BagNet(Bottleneck, [3, 4, 6, 3],
+                   strides=strides,
+                   kernel3=[1, 1, 1, 1],
+                   **kwargs)
     if pretrained:
         model.load_state_dict(model_zoo.load_url(model_urls['bagnet33']))
     return model
+
 
 def create_bagnet17(pretrained=False, strides=[2, 2, 2, 1], **kwargs):
     """Constructs a Bagnet-17 model.
@@ -142,10 +206,14 @@ def create_bagnet17(pretrained=False, strides=[2, 2, 2, 1], **kwargs):
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
-    model = BagNet(Bottleneck, [3, 4, 6, 3], strides=strides, kernel3=[1,1,1,0], **kwargs)
+    model = BagNet(Bottleneck, [3, 4, 6, 3],
+                   strides=strides,
+                   kernel3=[1, 1, 1, 0],
+                   **kwargs)
     if pretrained:
         model.load_state_dict(model_zoo.load_url(model_urls['bagnet17']))
     return model
+
 
 def create_bagnet9(pretrained=False, strides=[2, 2, 2, 1], **kwargs):
     """Constructs a Bagnet-9 model.
@@ -153,7 +221,36 @@ def create_bagnet9(pretrained=False, strides=[2, 2, 2, 1], **kwargs):
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
-    model = BagNet(Bottleneck, [3, 4, 6, 3], strides=strides, kernel3=[1,1,0,0], **kwargs)
+    model = BagNet(Bottleneck, [3, 4, 6, 3],
+                   strides=strides,
+                   kernel3=[1, 1, 0, 0],
+                   **kwargs)
     if pretrained:
         model.load_state_dict(model_zoo.load_url(model_urls['bagnet9']))
     return model
+
+
+if __name__ == '__main__':
+    import torch
+    from fastai.datasets import URLs, untar_data
+    from fastai.vision import ImageDataBunch, cnn_learner
+    # from fastai.vision import create_cnn
+    from fastai.metrics import accuracy
+
+    path = untar_data(URLs.MNIST_SAMPLE)
+    mnist = ImageDataBunch.from_folder(path)
+
+    # bagnet = create_bagnet9(num_classes=len(mnist.classes))
+    bagnet = create_bagnet9(pretrained=True)
+
+
+    bagnet = bagnet.cuda()
+    learner = cnn_learner(
+        data=mnist,
+        # mmkay I definitely wouldn't've guessed this
+        base_arch=lambda _: bagnet,
+        metrics=accuracy
+    )
+    # model = create_bagnet9(pretrained=True)
+    # print(model)
+    learner.fit_one_cycle(cyc_len=20, max_lr=1e-2)
